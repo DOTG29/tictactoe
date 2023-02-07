b422f191276de67548ccc19edaad27af0a27b965
DOTG29 <dgoris@edenschool.fr>
2023-02-07 16:57:04 +0100

    develop

---
+++ solo.js
@@ -0,0 +1,34 @@
+let casse = document.getElementsByClassName('casse')
+
+// for (let i = 0; i < casse.length; i++) {
+//     casse[i].addEventListener('click', () => {
+//         casse[i].style.backgroundImage = "url(img/icon-x.svg)"
+//         casse[i].style.backgroundRepeat = "no-repeat";
+//         casse[i].style.backgroundPosition = "center";
+//     });
+// }
+
+for (let i = 0; i < casse.length; i++) {
+    casse[i].addEventListener('click', () => {
+        casse[i].style.backgroundImage = "url(img/icon-o.svg)"
+        casse[i].style.backgroundRepeat = "no-repeat";
+        casse[i].style.backgroundPosition = "center";
+    });
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
